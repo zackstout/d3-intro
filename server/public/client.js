932ec536c01a372a34c553657357d1f3ee0d1118
Zack Stout <zackstout@gmail.com>
2018-03-07 23:47:27 -0600

    adding some more datasets to play with

--- server/public/client.js
+++ server/public/client.js
@@ -9,49 +9,92 @@ var ib = [];
 var django = [];
 var jackie = [];
 
+var domgrosses = [];
+var budgets = [];
+var years = [];
+
 var all = ['Reservoir Dogs', 'Pulp Fiction', "Kill Bill: Vol. 1", "Kill Bill: Vol. 2", "Inglorious Basterds", "Django Unchained", "Jackie Brown"];
 
 var graphCount = 0;
 
+var allBech = [];
+
 $(document).ready(function() {
   // Remember, you need a server to use this:
-  d3.csv('tarantino.csv', function(res) {
-    // console.log(res);
-    switch(res.movie) {
-      case "Reservoir Dogs": dogs.push(res);
-      break;
-      case "Pulp Fiction": pulp.push(res);
-      break;
-      case "Kill Bill: Vol. 1": kb1.push(res);
-      break;
-      case "Kill Bill: Vol. 2": kb2.push(res);
-      break;
-      case "Inglorious Basterds": ib.push(res);
-      break;
-      case "Django Unchained": django.push(res);
-      break;
-      case "Jackie Brown": jackie.push(res);
-      break;
-    }
-  }).then(function() {
-    var dogsSwears = sanitizeMovie(dogs);
-    graphMovie(dogsSwears);
 
-    var pulpSwears = sanitizeMovie(pulp);
-    graphMovie(pulpSwears);
-
-    var kb1Swears = sanitizeMovie(kb1);
-    graphMovie(kb1Swears);
-    var kb2Swears = sanitizeMovie(kb2);
-    graphMovie(kb2Swears);
-    var ibSwears = sanitizeMovie(ib);
-    graphMovie(ibSwears);
-    var djangoSwears = sanitizeMovie(django);
-    graphMovie(djangoSwears);
-    var jackieSwears = sanitizeMovie(jackie);
-    graphMovie(jackieSwears);
+  // All right this one should be fun too:
+  d3.csv('csvs/unisex_names_table.csv', function(res) {
+    console.log(res);
   });
+
+
+  // This one too:
+  // d3.csv('csvs/drug-use-by-age.csv', function(res) {
+  //   console.log(res);
+  // });
+
+  // This will be fun too:
+  // d3.csv('csvs/transfers.csv', function(res) {
+  //   console.log(res);
+  // });
+
+  // This will be a good one:
+  // d3.csv('csvs/KSEA.csv', function(res) {
+  //   console.log(res);
+  // });
+
+  // BECHDEL:
+  // d3.csv('csvs/movies.csv', function(res) {
+  //   // console.log(res);
+  //   allBech.push(res);
+  //   years.push(parseInt(res.year));
+  //   budgets.push(parseInt(res.budget_2013$));
+  //   domgrosses.push(parseInt(res.domgross_2013$));
+  // }).then(function() {
+  //   console.log(allBech);
+  //   graphBech(allBech);
+  // });
+
+  // TARANTINO:
+  // d3.csv('csvs/tarantino.csv', function(res) {
+  //   // console.log(res);
+  //   switch(res.movie) {
+  //     case "Reservoir Dogs": dogs.push(res);
+  //     break;
+  //     case "Pulp Fiction": pulp.push(res);
+  //     break;
+  //     case "Kill Bill: Vol. 1": kb1.push(res);
+  //     break;
+  //     case "Kill Bill: Vol. 2": kb2.push(res);
+  //     break;
+  //     case "Inglorious Basterds": ib.push(res);
+  //     break;
+  //     case "Django Unchained": django.push(res);
+  //     break;
+  //     case "Jackie Brown": jackie.push(res);
+  //     break;
+  //   }
+  // }).then(function() {
+  //   var dogsSwears = sanitizeMovie(dogs);
+  //   graphMovie(dogsSwears);
+  //
+  //   var pulpSwears = sanitizeMovie(pulp);
+  //   graphMovie(pulpSwears);
+  //
+  //   var kb1Swears = sanitizeMovie(kb1);
+  //   graphMovie(kb1Swears);
+  //   var kb2Swears = sanitizeMovie(kb2);
+  //   graphMovie(kb2Swears);
+  //   var ibSwears = sanitizeMovie(ib);
+  //   graphMovie(ibSwears);
+  //   var djangoSwears = sanitizeMovie(django);
+  //   graphMovie(djangoSwears);
+  //   var jackieSwears = sanitizeMovie(jackie);
+  //   graphMovie(jackieSwears);
+  // });
 });
+
+
 
 
 // The goal here will be to collapse the data into minute-sized chunks, and radius of circle at that minute will depict number of swears:
@@ -83,6 +126,42 @@ function sanitizeMovie(movie) {
 
   return eventArray;
 }
+
+function graphBech(arr) {
+  var dom = d3.scaleLinear()
+    .domain([0, d3.max(domgrosses)])
+    .range([10, 790]);
+  var bud = d3.scaleLinear()
+    .domain([0, d3.max(budgets)])
+    .range([10, 790]);
+  var year = d3.scaleLinear()
+    .domain([d3.min(years), d3.max(years)])
+    .range([10, 590]);
+
+  var circles = d3.select(".bechdel")
+    .selectAll("circle")
+    .data(arr)
+    .enter().append("circle")
+    .style("fill", function(d) {
+      return d.binary == 'PASS' ? 'blue' : 'red';
+    })
+    .attr('r', 5)
+    .attr('cx', function(d) {
+      // return dom(parseInt(d.domgross_2013$));
+      return bud(parseFloat(d.budget_2013$));
+
+    })
+    .attr('cy', function(d) {
+      // return bud(parseFloat(d.budget_2013$));
+      return year(parseInt(d.year));
+    });
+}
+
+
+
+
+
+
 
 
 // ok the issue is likely that we're selecting *all* circles, so it only works the first time...
